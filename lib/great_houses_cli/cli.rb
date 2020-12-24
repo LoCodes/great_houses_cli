8865681c020562ea2dfa89d6c3ebda0ea7a31d04
Loren Cipriano <loren25cipriano@gmail.com>
2020-12-23 21:09:36 -0800

    updated start method, and created greeting method

--- lib/great_houses_cli/cli.rb
+++ lib/great_houses_cli/cli.rb
@@ -3,8 +3,16 @@
 class CLI 
 
     def start
-        puts "Welcome to the Seven Kingdoms, may I fancy thou information of the nine great houses of Westeros?"
-        puts "What is thy name traveler?"
+        puts "Welcome to the Seven Kingdoms, which region would thou like to visit?"
+        puts "Thy name is needed before entering traveler" #i need your name before you enter, What is thy name traveler?
+    end 
+
+    def user_input
+        gets.strip
+    end 
+
+    def greeting(name)
+        puts "Very well #{name}, may I fancy thou information of the nine great houses of Westeros?"
     end 
 
 
