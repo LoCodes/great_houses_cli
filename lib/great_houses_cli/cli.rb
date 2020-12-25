7ac295aef864a3453072bc65629d9794150f06fe
Loren Cipriano <loren25cipriano@gmail.com>
2020-12-25 13:38:51 -0800

    edited 'exit' to 'n' in greeting

--- lib/great_houses_cli/cli.rb
+++ lib/great_houses_cli/cli.rb
@@ -17,7 +17,7 @@ class CLI
 
     def greeting(name)
         puts "Very well #{name}, may I fancy thou information of the nine great houses of the Seven Kingdoms?"
-        puts "please input 'y' or 'exit to leave the Seven Kingdoms"
+        puts "please input 'y' or 'n' to leave the Seven Kingdoms"
         menu 
     end 
 
