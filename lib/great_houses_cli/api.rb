0c1bda4a9da866ce086f88bbb6339b40ddd5a01a
Loren Cipriano <loren25cipriano@gmail.com>
2021-01-06 17:40:37 -0800

    deleted the menu(recursion) in menu and can now exit program right away

--- lib/great_houses_cli/api.rb
+++ lib/great_houses_cli/api.rb
@@ -3,7 +3,6 @@
 # creating a new RUBY objects with that data => Object Orientation 
 # should always be class methods! dont need instance methods 
 
-require 'pry'
 
 
 class API 
@@ -14,16 +13,9 @@ class API
         houses_array = JSON.parse(response)
         houses_array.each do |house|
             House.new(house)
+            #binding.pry
         end 
-        # houses_array = House.new 
-        # houses_array.name = house["id"]
-        # houses_array.region = house["region"]
-        # houses_array.seat = house["seat"]
-        # houses_array.allegiance = house["allegiance"]
-        # houses_array.sigil = house["sigil"]
-        # houses_array.words = house["words"]
     end 
 
 end 
 
-    # :name, :region, :seat, :allegiance, :sigil, :words
--- lib/great_houses_cli/cli.rb
+++ lib/great_houses_cli/cli.rb
@@ -4,7 +4,7 @@ class CLI
 
     def start
         puts "Welcome to the Seven Kingdoms, which region would thou like to visit?"
-        puts "Thy name is needed before entering traveler" #i need your name before you enter, What is thy name traveler?
+        puts "Thy name is needed before entering traveler." #i need your name before you enter, What is thy name traveler?
         API.get_data
         greeting(user_input)
         
@@ -16,7 +16,7 @@ class CLI
 
     def greeting(name)
         puts "Very well #{name}, may I fancy thou information of the great houses of the Seven Kingdoms?"
-        puts "please input 'y' or 'n' to leave the Seven Kingdoms" 
+        puts "please input 'y' or 'n' to leave the Seven Kingdoms." 
 
         menu 
     end 
@@ -31,7 +31,7 @@ class CLI
     end 
 
     def invalid 
-        puts "Invalid entry, try again."
+        puts "There is no such house, try again or 'n' to exit."
         menu #- can be added here as well 
     end 
 
@@ -50,7 +50,7 @@ class CLI
         #binding.pry
         puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
         puts "Name: #{house.name}"
-        puts "Region: #{house.region[0]}"#.gsub!(/[!@%&"]/,'')
+        puts "Region: #{house.region[0]}" if house.region.join != ""
         puts "Seat: #{house.seat.join(", ")}" if house.seat.join != "" 
         puts "Allegiance: #{house.allegiance.join(", ")}" if house.allegiance.join != ""
         puts "Sigil: #{house.sigil}" if house.sigil != ""
@@ -70,7 +70,7 @@ class CLI
 
         if selection == 'y'
             houses_list
-            menu            #recursion 
+            #menu            #recursion 
         elsif selection == 'n'
             bye
         else 
